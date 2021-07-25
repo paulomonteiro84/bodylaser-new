017b684117bf29f862e3543fc04e60a391bf62d8
paulomonteiro84 <paulomonteiro84@gmail.com>
2021-07-25 16:37:25 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/foundation.min.css">
+    <link rel="stylesheet" href="css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+</head>
+<body>
+
+<div class="site-container">
+    <div class="grid-x top-bar bodylaser-blue">
+        <div class="grid-container">
+            <div class="grid-x">
+                <div class="cell small-12 text-center"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grid-container">
+        <div class="grid-x top-header align-justify">
+            <div class="cell auto shrink medium-4 text-left logo"><img src="img/logo.png"></div>
+            <div class="cell medium-4 text-center hide-for-small-only align-center">Morada</div>
+            <div class="cell medium-4 text-right hide-for-small-only">Marcação</div>
+            <div class="cell auto shrink text-right mobile-menu show-for-small-only">svg humburger</div>
+        </div>
+    </div>
+    <div class="grid-x hero hero-section">
+        <div class="cell small-12">
+            <div class="hero-placeholder"></div>
+        </div>
+    </div>
+    <div class="grid-container">
+        <div class="grid-x align-center grid-margin-x grid-margin-y">
+            <div class="cell small-4 text-center medium-3">
+                <a href="https://www.instagram.com/clinica_bodylaser_oficial/">
+                    <div class="__block instagram">
+                        <div class="link-icon"><img src="img/link-icon.png" alt=""></div>
+                        <img class="icon" src="img/insta.png" alt="">
+                        <span class="small-text">Siga o nosso</span>
+                        <img src="img/insta-logo.png" alt="" class="logo">
+                        <span class="big-text">@clinica_bodylaser_oficial</span>
+                    </div>
+                </a>
+            </div>
+            <div class="cell small-4 text-center medium-3">
+                <a href="wa.me/+351969291459">
+                    <div class="__block wup">
+                        <div class="link-icon"><img src="img/link-icon.png" alt=""></div>
+                        <img class="icon" src="img/wup.png" alt="">
+                        <span class="small-text">Estamos no</span>
+                        <img src="img/wup-logo.png" alt="" class="logo">
+                        <span class="big-text">96 929 14 59</span>
+                    </div>
+                </a>
+            </div>
+            <div class="cell small-4 text-center medium-3">
+                <a href="https://goo.gl/maps/18E1Cys6CZJABtxr6">
+                    <div class="__block maps">
+                        <div class="link-icon"><img src="img/link-icon.png" alt=""></div>
+                        <img class="icon" src="img/maps.png" alt="">
+                        <span class="small-text">Morada</span>
+                        <span class="big-text">Av. Eng. Duarte Pacheco 19 Sala 10<br>1070-100 LISBOA</span>
+                        <span class="small-text">(junto ao “Médico dos Dentes”)</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="grid-x section happy-clients-section">
+        <div class="cell small-12">
+            <div class="grid-container">
+                <div class="grid-x text-center">
+                    <div class="cell small-12">
+                        <div class="section-title">Clientes satisfeitos 💖</div>
+                    </div>
+                </div>
+                <div class="grid-x align-center">
+                    <div class="cell small-12 medium-8 large-6">
+                        <div class="swiper-wrap">
+                            <div class="swiper-container testimonial-swiper">
+                                <div class="swiper-wrapper">
+                                    <div class="swiper-slide testimonial">
+                                        <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, Amet
+                                            autem
+                                            consequuntur cumque, distinctio dolorum</p>
+                                        <div class="name">Sieira</div>
+                                    </div>
+                                    <div class="swiper-slide testimonial">
+                                        <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, Amet
+                                            autem
+                                            consequuntur cumque, distinctio dolorum</p>
+                                        <div class="name">Susanira</div>
+                                    </div>
+                                    <div class="swiper-slide testimonial">
+                                        <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, Amet
+                                            autem
+                                            consequuntur cumque, distinctio dolorum</p>
+                                        <div class="name">Susana Vieira</div>
+                                    </div>
+                                </div>
+                                <!-- If we need navigation buttons -->
+
+                            </div>
+                            <div class="swiper-button-prev prev-happy-clients"></div>
+                            <div class="swiper-button-next next-happy-clients"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="grid-x section treatment-depil">
+        <div class="grid-container">
+            <div class="grid-x text-center">
+                <div class="cell small-12">
+                    <div class="section-title">Tratamentos de estética</div>
+                </div>
+            </div>
+            <div class="grid-x align-center treatment">
+                <div class="cell small-12 medium-8">
+                    <div class="grid-x grid-padding-x align-center-middle">
+                        <div class="cell small-12 medium-6">
+                            <video controls autoplay muted loop src="img/depilacao-laser-video.mp4"></video>
+                        </div>
+                        <div class="cell small-12 medium-6">
+                            <div class="service-title">💎 Depilação Laser</div>
+                            <div class="service-description"><p>As nossas máquinas são equipadas com um triplo laser de
+                                    3
+                                    comprimentos
+                                    de onda diferentes para um resultado 100% eficaz.</p>
+                                ✅ Para todos os tipos de pele.
+                                <br>
+                                ✅ Resultados na primeira sessão.
+                                <br>
+                                ✅ Sem dor, máxima segura.
+                            </div>
+                            <a href="tratamentos/depilacao-triplo-laser">
+                                <div class="btn">Agende a sua depilação</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="grid-x section treatment-fat">
+        <div class="grid-container">
+            <div class="grid-x text-center">
+                <div class="cell small-12">
+                    <div class="section-title">Para a eliminação da gordura localizada</div>
+                </div>
+            </div>
+            <div class="grid-x align-center treatment">
+                <div class="cell small-12 medium-8">
+                    <div class="grid-x grid-padding-x align-center-middle">
+                        <div class="cell small-12 medium-6">
+                            <img src="img/crio.jpg ">
+                        </div>
+                        <div class="cell small-12 medium-6">
+                            <div class="service-title">💎 Criolipolise</div>
+                            <div class="service-description"><p>As nossas máquinas são equipadas com um triplo laser de
+                                    3
+                                    comprimentos
+                                    de onda diferentes para um resultado 100% eficaz.</p>
+                                ✅ Para todos os tipos de pele.
+                                <br>
+                                ✅ Resultados na primeira sessão.
+                                <br>
+                                ✅ Sem dor, máxima segura.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="grid-x align-center treatment">
+                <div class="cell small-12 medium-8">
+                    <div class="grid-x grid-padding-x align-center-middle">
+                        <div class="cell small-12 medium-6">
+                            <div class="service-title">💎 Mesoterapia</div>
+                            <div class="service-description"><p>As nossas máquinas são equipadas com um triplo laser de
+                                    3
+                                    comprimentos
+                                    de onda diferentes para um resultado 100% eficaz.</p>
+                                ✅ Para todos os tipos de pele.
+                                <br>
+                                ✅ Resultados na primeira sessão.
+                                <br>
+                                ✅ Sem dor, máxima segura.
+                            </div>
+                        </div>
+                        <div class="cell small-12 medium-6">
+                            <img src="img/meso.jpg">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+<script src="js/jquery-3.6.0.js"></script>
+<script src="js/swiper-bundle.min.js"></script>
+<script src="js/app.js"></script>
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;600&display=swap" rel="stylesheet">
+</body>
+</html>
